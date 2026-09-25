33700cc14df72e01c49ce7591c11becbb871f345
DoraDi <kananeu0411@gmail.com>
2026-09-25 13:00:54 +0500

    надеюсь все

--- Resume/js/task2.js
+++ Resume/js/task2.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById("task2Btn");
 const classes = document.getElementById("classes");
 
-btn.addEventListener("click", function () {
-    btn.classList.toggle("active");
-    console.log(btn.classList);
-    classes.textContent = btn.className;
-});
+if (btn && classes) {
+    btn.addEventListener("click", function () {
+        btn.classList.toggle("active");
+        console.log(btn.classList);
+        classes.textContent = btn.className;
+    });
+}
