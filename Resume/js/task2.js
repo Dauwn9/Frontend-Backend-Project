1f465b5553edbbb6f10b553fbaeb3fa3aa469a70
DoraDi <kananeu0411@gmail.com>
2026-09-25 13:08:08 +0500

    пвдв

--- Resume/js/task2.js
+++ Resume/js/task2.js
@@ -1,10 +1,13 @@
-const btn = document.getElementById("task2Btn");
-const classes = document.getElementById("classes");
+(function () {
+    const btn = document.getElementById("task2Btn");
+    const classes = document.getElementById("classes");
 
-if (btn && classes) {
+    if (!btn || !classes) return;
+
     btn.addEventListener("click", function () {
         btn.classList.toggle("active");
-        console.log(btn.classList);
+
+        console.log(btn.className);
         classes.textContent = btn.className;
     });
-}
+})();
